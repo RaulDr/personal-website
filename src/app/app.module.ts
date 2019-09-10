a148c394a7b1d61e0213e8a8ea3e915928233eef
Dragoiu, Raul <raul.dragoiu@in-tech.com>
2019-09-11 00:14:17 +0300

    add navbar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { MainComponent } from './main/main.component';
+import {MainComponent} from './main/main.component';
+import {NavbarComponent} from './global/navbar/navbar.component';
+import { FooterComponent } from './global/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +23,5 @@ import { MainComponent } from './main/main.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
